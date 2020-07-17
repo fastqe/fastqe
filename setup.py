fa3d61bc2d199fc96b550fe84bb5025bb11cb19e
lonsbio <andrew.lonsdale@lonsbio.com.au>
2020-07-17 23:36:56 +1000

    Switch to setuptools

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 LONG_DESCRIPTION = \
 '''The program reads one or more input FASTQ files.
 For each file it computes the minimum, maximum and mean FASTQ quality score at each position across all reads in a file.
@@ -10,7 +10,7 @@ For some reason, it then represents these as emoji.
 setup(
   name = 'fastqe',
   packages = ['fastqe'],
-  version = '0.2.5',
+  version = '0.2.6',
   license = 'MIT',
   description = 'A emoji based bioinformatics command line tool',
   long_description=(LONG_DESCRIPTION),
