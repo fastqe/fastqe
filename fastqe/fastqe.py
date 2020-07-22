43ea4db05a51b747659d616286ccc1aa1bd16161
cjwomack <39180413+cjwomack@users.noreply.github.com>
2020-07-23 00:37:51 +1000

    run_fastqe for jupyter notebooks
    Implementing one of the suggestions of Issue #14 ... so this function can be used in jupyter notebooks

--- fastqe/fastqe.py
+++ fastqe/fastqe.py
@@ -533,6 +533,14 @@ def init_logging(log_filename):
         logging.info('program started')
         logging.info('command line: {0}'.format(' '.join(sys.argv)))
 
+def run_fastqe(fasta_files,minlen=0,scale=False,version=False,
+               mean = True,custom=None,noemoji=False,min=False,max=False,
+               output=None,long=None,log=None,bin=False):
+    options = Namespace(bin=bin, custom=custom, fasta_files=fasta_files,log=log, long=long, max=max, mean=mean, min=min, minlen=minlen, noemoji=noemoji, output=output, scale=scale, version=version)
+    if options.version:
+        print(PROGRAM_NAME,PROGRAM_VERSION)
+        return
+    process_files(options)
 
 def main():
     "Orchestrate the execution of the program"
