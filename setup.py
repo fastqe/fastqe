15188ab4f8b4b98599321139833ad329d7e62e97
Andrew Lonsdale <Andrew.Lonsdale@petermac.org>
2023-06-28 15:27:43 +1000

    Cleanup for ver 0.3.0 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 # Remember to change when making a new release
-version = '0.2.7'
+version = '0.3.0'
 dl_version = 'master' if 'dev' in version else 'v{}'.format(version)
 
 with open('README.md') as f:
