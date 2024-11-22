c205d856aea0b0714ee5f80bf2ce2b1ebf23ccb7
Adam Taranto <adam.p.taranto@gmail.com>
2024-11-22 22:06:32 +1100

    fix colour logging

--- src/fastqe/utils/logs.py
+++ src/fastqe/utils/logs.py
@@ -1,46 +1,11 @@
 import logging
 import sys
 
-def init_logging(loglevel="DEBUG", logfile=None):
-    """
-    Initializes the logging system with a specified log level and custom formatter.
-
-    Parameters:
-    loglevel (str): The log level to use (e.g., "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL").
-                    Defaults to "DEBUG".
-    logfile (str, optional): The file to which log messages should be written. If None, log messages
-                             will only be output to stderr. Defaults to None.
-
-    Raises:
-    ValueError: If the provided log level is invalid.
-    """
-    # Convert log level string to numeric value
-    numeric_level = getattr(logging, loglevel.upper(), None)
-    if not isinstance(numeric_level, int):
-        raise ValueError(f"Invalid log level: {loglevel}")
-
-    # Define log message format
-    fmt = "%(asctime)s | %(levelname)s | %(message)s"
-
-    # Create a StreamHandler to output log messages to stderr
-    handler_sh = logging.StreamHandler(sys.stderr)
-    handler_sh.setFormatter(logging.Formatter(fmt))
-
-    # Configure the logging system
-    if logfile is not None:
-        # Create a FileHandler to output log messages to a file
-        handler_fh = logging.FileHandler(logfile)
-        handler_fh.setFormatter(logging.Formatter(fmt))
-        logging.basicConfig(format=fmt, level=numeric_level, handlers=[handler_sh, handler_fh])
-    else:
-        logging.basicConfig(format=fmt, level=numeric_level, handlers=[handler_sh])
-
 class CustomFormatter(logging.Formatter):
     """
     Custom logging formatter to add color to log messages based on their severity level.
     """
 
-    # Adapted from https://alexandra-zaharia.github.io/posts/make-your-own-custom-color-formatter-with-python-logging
     # ANSI escape codes for colors
     grey = "\x1b[38;21m"
     blue = "\x1b[38;5;39m"
@@ -79,3 +44,37 @@ class CustomFormatter(logging.Formatter):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
+
+def init_logging(loglevel="DEBUG", logfile=None):
+    """
+    Initializes the logging system with a specified log level and custom formatter.
+
+    Parameters:
+    loglevel (str): The log level to use (e.g., "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL").
+                    Defaults to "DEBUG".
+    logfile (str, optional): The file to which log messages should be written. If None, log messages
+                             will only be output to stderr. Defaults to None.
+
+    Raises:
+    ValueError: If the provided log level is invalid.
+    """
+    # Convert log level string to numeric value
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f"Invalid log level: {loglevel}")
+
+    # Define log message format
+    fmt = "%(asctime)s | %(levelname)s | %(message)s"
+
+    # Create a StreamHandler to output log messages to stderr
+    handler_sh = logging.StreamHandler(sys.stderr)
+    handler_sh.setFormatter(CustomFormatter(fmt))
+    
+    # Configure the logging system
+    if logfile is not None:
+        # Create a FileHandler to output log messages to a file
+        handler_fh = logging.FileHandler(logfile)
+        handler_fh.setFormatter(logging.Formatter(fmt))
+        logging.basicConfig(format=fmt, level=numeric_level, handlers=[handler_sh, handler_fh])
+    else:
+        logging.basicConfig(format=fmt, level=numeric_level, handlers=[handler_sh])
